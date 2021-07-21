cc93db06462f174bcba5b104f6e13e2d36059d08
Sunnyeunice <aburvella@gmail.com>
2021-07-20 23:07:04 -0400

    Added

--- src/assignment2/BinarySearchTree.java
+++ src/assignment2/BinarySearchTree.java
@@ -1,4 +1,62 @@
 package assignment2;
 
 public class BinarySearchTree {
+    private Node root;
+
+    /**
+     * Inserts a key into this binary search tree.
+     * If there are n nodes in the tree, then the average runtime of
+     this method should be log(n).
+     *
+     * @param key The key to insert.
+     */
+    public void insert(int key) {
+        insert(this.root, key);
+    }
+
+    private void insert(Node root, int key){
+        if(root == null){
+            root = new Node();
+            root.key = key;
+            return;
+        }
+        if(root.key < key){
+            insert(root.right, key);
+        }else{
+            insert(root.left, key);
+        }
+    }
+
+    /**
+     * Checks whether or not a key exists in this binary search tree.
+     * If there are n nodes in the tree, then the average runtime of
+     this method should be log(n).
+     *
+     * @param key The key to check for.
+     * @return true if the key is present in this binary search tree,
+    false otherwise.
+     */
+
+    public boolean find(int key) {
+        return find(this.root, key);
+    }
+
+    private boolean find(Node root, int key){
+        if(root == null){
+            return false;
+        }
+        if(root.key < key){
+            return find(root.right, key);
+        }
+        else if (root.key > key){
+            return find(root.left, key);
+        }
+        return true;
+    }
+
+    public static class Node {
+        public int key;
+        public Node left;
+        public Node right;
+    }
 }
--- src/assignment2/ListPhoneBook.java
+++ src/assignment2/ListPhoneBook.java
@@ -1,4 +1,41 @@
 package assignment2;
 
+import java.util.ArrayList;
 public class ListPhoneBook {
+    private ArrayList<Entry> book;
+
+    public int size(){
+        return this.book.size();
+    }
+
+    public void insert(String name, long phoneNumber){
+        if(this.book == null){
+            this.book = new ArrayList<Entry>();
+        }
+        this.book.add(new Entry(name, phoneNumber));
+    }
+
+    public long find(String name){
+        //handle case when the list is empty
+
+        for(int i = 0; i < this.book.size(); ++i){
+            Entry entry = this.book.get(i);
+            if(entry.name.equals(name)){
+                return entry.phoneNumber;
+            }
+        }
+        //handle case when name does not exist
+        // for now, returning 0 means not found
+        return 0L;
+    }
+
+    public class Entry {
+        public String name;
+        public long phoneNumber;
+
+        public Entry(String name, long phoneNumber){
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+    }
 }
